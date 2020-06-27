e8fe8d26d7f8cc257115d4edd47f2acab186e04d
James Maguire <3rdmaguire@gmail.com>
2020-06-27 18:03:59 +1000

    Split index into categories

--- static/search.js
+++ static/search.js
@@ -3,17 +3,19 @@ function indexFilter() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById('filterInput');
     filter = input.value.toUpperCase();
-    ul = document.getElementsByClassName("org-ul")[0];
-    li = ul.getElementsByTagName('li');
+    ul = document.getElementsByClassName("org-ul");
 
     // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+    for (u = 0; u < ul.length; u++) {
+        li = ul[u].getElementsByTagName('li');
+        for (i = 0; i < li.length; i++) {
+            a = li[i];
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
         }
     }
 }
