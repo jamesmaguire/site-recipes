c41b2b38f1533967e1910d290269c23d96468e60
James Maguire <3rdmaguire@gmail.com>
2020-10-11 10:03:00 +1100

    Don't cache index

--- static/sw.js
+++ static/sw.js
@@ -2,7 +2,6 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
-       '/',
        '/static/common.css',
        '/static/index.css',
        '/static/recipe.css',
