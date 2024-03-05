039a3e23abeac309a6e8a9af9259934b37bc7652
jamesmaguire <3rdmaguire@gmail.com>
2024-03-05 14:08:51 +1100

    Add checks to method section

--- static/recipe.js
+++ static/recipe.js
@@ -1,6 +1,7 @@
 // Add ingredients tag to ingredients outline (for styling)
 // Add ul li wrapper for checkboxes
 window.onload = function addCSSClasses() {
+    // Add check boxes to ingredients
     let ingredientdiv = document.getElementsByClassName("outline-2")[0];
     ingredientdiv.className = "outline-2 ingredients";
     ingredients = document.querySelectorAll(".ingredients li");
@@ -8,12 +9,20 @@ window.onload = function addCSSClasses() {
         ingredients[i].innerHTML = '<label class="check"><input type="checkbox">'
             + '<span></span>' + ingredients[i].innerHTML + '</label>';
     }
+    // Add check boxes to method
+    let methoddiv = document.getElementsByClassName("outline-2")[1];
+    methoddiv.className = "outline-2 method";
+    method = document.querySelectorAll(".method li");
+    for (let i=0; i<method.length; i++) {
+        method[i].innerHTML = '<label class="check"><input type="checkbox">'
+            + '<span></span>' + method[i].innerHTML + '</label>';
+    }
 };
 
 // Tickbox functions
 
 function copyTicked() {
-    let items = document.querySelectorAll(".check");
+    let items = document.querySelectorAll(".ingredients .check");
     let text = "";
     for (let i=0; i<items.length; i++) {
         let item = items[i].getElementsByTagName("input")[0];
